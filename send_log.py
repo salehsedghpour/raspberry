39f74dca1c1cf965f8f94a75ed9e7cef1c865fa8
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2019-04-15 21:26:00 +0430

    Update send_log.py

--- send_log.py
+++ send_log.py
@@ -30,12 +30,12 @@ def send_log_to_mongo(user,zone,mongo_user,mongo_pass,mongo_url,data,miner_id):
     try:
         myclient = pymongo.MongoClient("mongodb://" + mongo_user + ":" + mongo_pass + "@" + mongo_url)
         mydb = myclient["monitoring"]
-        mycol = mydb[user]
-        mydict = {"username" : user,
-        "zone_id" : zone,
-        "miner_id" : int(miner_id),
-        "timestamp" : time.time(),
-        "log" :data}
+        mycol = mydb['vahid']
+        mydict = {"username" : 'vahid',
+        "zone_id" : 1,
+        "miner_id" : 1,
+        "timestamp" : time.time()
+        #"log" :data}
         x = mycol.insert_one(mydict)
     except:
         print("There is some error in sending data to miner")
