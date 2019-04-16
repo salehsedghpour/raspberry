889086c0616bb2ff570c383cbf9f484c7c53c4eb
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2019-04-16 11:43:59 +0430

    Update my-bash.sh

--- my-bash.sh
+++ my-bash.sh
@@ -2,7 +2,7 @@
 echo synching with git.
 (cd /opt/codes/ ;git pull origin master)
 #(sudo -s ; apt install python3-pip)
-#/usr/bin/pip3 install requests
+pip3 install requests
 #pip3 install pymongo
 #echo { "zone":1, "user":"me","mongo-user":"my-mongo-user", "mongo-password":"NGEyY2IwZWQ5OGM1", "mongo-url":"mon.hcsone.net:27117/" } > /opt/auth.json
 #/usr/bin/python3.5 /opt/codes/synch_db.py
